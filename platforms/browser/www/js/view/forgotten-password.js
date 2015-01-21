49af0d608459f90e2cf1b026318bdedd424428a1
Shane Gibson <shane.a.gibson@gmail.com>
2015-01-21 18:56:37 +0000

    Added missing platform files

---
+++ platforms/browser/www/js/view/forgotten-password.js
@@ -0,0 +1,57 @@
+define(function(require) {
+
+  var Marionette = require('marionette');
+  var template = require('hbs!../html/reset-password');
+  var click = require('../util/click');
+  var vent = require('../util/vent');
+  var validator = require('../util/validator');
+  var serverGateway = require('../util/server-gateway');
+
+  var view = Marionette.LayoutView.extend({
+
+    tagName: "div",
+
+    template: template,
+
+    events: {
+      'click .request-reset' : 'requestPasswordReset',
+      'blur input.password' : 'validateField'
+    },
+
+    ui: {
+      emailAddressInput: 'input.emailaddress'
+    },
+
+    requestPasswordReset: click.single(function() {
+      if (this.validateForm()) {
+        this.submit();
+      }
+    }),
+
+    validateForm: function() {
+      return validator.validateFields($('.form').find('.field'));
+    },
+
+    validateField: function(e) {
+      validator.validateFields($(e.currentTarget));
+    },
+
+    submit: function() {
+      var self = this;
+      return serverGateway
+        .post('/account/password/forgotten', {
+          emailAddress: this.ui.emailAddressInput.val()
+        })
+        .then(function(response) {
+          // TODO - show email sent notification
+        })
+        .catch(function(response) {
+          window.plugins.toast.showLongCenter('Something unexpected happened. Please try again.');
+        });
+    }
+
+  });
+
+  return view;
+
+});
---
+++ platforms/browser/www/js/view/reset-password.js
@@ -0,0 +1,72 @@
+define(function(require) {
+
+  var Marionette = require('marionette');
+  var template = require('hbs!../html/reset-password');
+  var click = require('../util/click');
+  var vent = require('../util/vent');
+  var validator = require('../util/validator');
+  var serverGateway = require('../util/server-gateway');
+
+  var view = Marionette.LayoutView.extend({
+
+    tagName: "div",
+
+    template: template,
+
+    events: {
+      'click .reset' : 'resetPassword',
+      'blur input.password' : 'validateField'
+    },
+
+    ui: {
+      passwordInput: 'input.password'
+    },
+
+    resetPassword: click.single(function() {
+      if (this.validateForm()) {
+        this.submit();
+      }
+    }),
+
+    validateForm: function() {
+      return validator.validateFields($('.form').find('.field'));
+    },
+
+    validateField: function(e) {
+      validator.validateFields($(e.currentTarget));
+    },
+
+    submit: function() {
+      var self = this;
+      return serverGateway
+        .post('/account/password/reset', {
+          userId: localStorage.getItem('userid'),
+          password: this.ui.passwordInput.val()
+        })
+        .then(function(response) {
+          if (response.user.verified) {
+            vent.trigger('navigate', 'home');
+          } else {
+            vent.trigger('navigate', 'verify-mobile');
+          }
+        })
+        .catch(function(response) {
+          switch (response.status) {
+            case 409 : {
+              // TODO - show link already used error
+              break;
+            }
+            case 410 : {
+              // TODO - show link expired error
+              break;
+            }
+          }
+          window.plugins.toast.showLongCenter('Something unexpected happened. Please try again.');
+        });
+    }
+
+  });
+
+  return view;
+
+});
