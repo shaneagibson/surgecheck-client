c528d3e4d6fd2c357c3979c9747276f6b81e6bf2
Shane Gibson <shane.a.gibson@gmail.com>
2015-01-29 13:38:20 +0000

    Added event listener for sign out

--- app/scripts/view/profile.js
+++ app/scripts/view/profile.js
@@ -3,6 +3,7 @@ define('view/profile', function(require) {
   var Marionette = require('marionette');
   var template = require('hbs!../html/profile');
   var vent = require('../util/vent');
+  var context = require('./context');
 
   var view = Marionette.LayoutView.extend({
 
@@ -11,11 +12,19 @@ define('view/profile', function(require) {
     template: template,
 
     events: {
-      'click .icon-menu' : 'showMenu'
+      'click .icon-menu' : 'showMenu',
+      'click .sign-out' : 'signOut'
     },
 
     showMenu: function(){
       vent.trigger('menu:show', 'profile');
+    },
+
+    signOut: function(){
+      delete context.user;
+      delete context.session;
+      // TODO - logout of account-service
+      vent.trigger('menu:show', 'landing');
     }
 
   });
